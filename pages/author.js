91b453a8ad64b55f2761ca806037e439b404f959
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-08-05 11:27:58 +0300

    link

--- pages/author.js
+++ pages/author.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const author = () => {
 
-    const router = useRouter()
+ 
   return (
     <div className='home'>
         <h1 className='auth-title'>Choose your role:</h1>
         <div className="auth-options">
-          <div onClick={() => router.push('/user')} className='auth-option'>
-              <h1>User</h1>
-           </div>
-          <div onClick={() => router.push('/creator')} className='auth-option'>
-              <h1>Creator</h1>
-          </div>
+            <Link href='/user'>
+              <div className='auth-option'>
+               <h1>User</h1>
+              </div>
+           </Link>
+           <Link href='/creator'>
+             <div  className='auth-option'>
+               <h1>Creator</h1>
+            </div>
+          </Link>
         </div>
     </div>
   )
