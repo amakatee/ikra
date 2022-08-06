71344280f1dc4fd53fe50b8723a78e248f225eb0
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-08-06 18:24:03 +0200

    Initial commit

--- pages/user.js
+++ pages/user.js
@@ -2,9 +2,40 @@ import React from 'react'
 import { Layout } from '../components/Layout'
 
 const User = () => {
+   
+  const quizs = [{
+    id: '1',
+    question:'vopros',
+    answers: [
+      {
+      id:"1",
+      answer:'option1'
+      },
+      {
+        id:"2",
+        anser:'option2'
+      }
+    ]
+
+
+  }]
+
   return (
     <Layout>
-        <div> userView</div>
+        <section className='quiz-section'>
+          <div className='quiz-cont'>
+           {quizs.map(q => (
+             <div className='quest-box'>{q.question}</div>
+             
+           ))}
+           
+
+            <div className='quiz-opts'>
+              <div>answer 1</div>
+              <div>answer2</div>
+            </div>
+          </div>
+        </section>
 
     </Layout>
     
